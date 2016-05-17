660deb8bdcfa83b5580f11d1a3a8d206811c42d6
kalantaev <nitrys@mail.ru>
2016-05-17 19:57:35 +0300

    The implementation of methods for spring hibernate template

--- src/main/java/ru/hibernate/hibernateTemplate/UserDAOHibTemp.java
+++ src/main/java/ru/hibernate/hibernateTemplate/UserDAOHibTemp.java
@@ -1,5 +1,6 @@
 package ru.hibernate.hibernateTemplate;
 
+import org.hibernate.SessionFactory;
 import org.hibernate.mapping.Collection;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
@@ -7,23 +8,20 @@ import org.springframework.stereotype.Service;
 import ru.DAO.UserDAO;
 import ru.entity.User;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Kalantaev Alexandr on 15.05.2016.
  */
-//@Service
+
 public class UserDAOHibTemp extends HibernateDaoSupport implements UserDAO{
+
+
     @Override
     public List<User> selectSortedByLoginUser() {
         return null;
     }
-
-//    public Collection loadProductsByCategory(String category) throws
-//            DataAccessException {
-//        return this.hibernateTemplate.find("from test.Product product where
-//                product.category=?", category);
-//    }
 
 
     @Override
@@ -38,26 +36,30 @@ public class UserDAOHibTemp extends HibernateDaoSupport implements UserDAO{
 
     @Override
     public User selectByLogin(String login) {
-        return null;
+
+        return ((List<User>)getHibernateTemplate().find("from User where login = ?", login)).get(0);
     }
 
     @Override
     public List<User> selectAllUser() {
-        return null;
+        return (List<User>) getHibernateTemplate().find("from User");
     }
 
     @Override
     public void deleteUserByID(Integer id) {
-
+        User user = selectByID(id);
+        getHibernateTemplate().delete(user);
     }
 
     @Override
     public void updateUser(User user) {
-
+        getHibernateTemplate().saveOrUpdate(user);
     }
 
     @Override
     public void updateLastVisitByID(Integer id) {
-
+        User user = selectByID(id);
+        user.setDateLastVisit(new Date());
+        getHibernateTemplate().saveOrUpdate(user);
     }
 }
